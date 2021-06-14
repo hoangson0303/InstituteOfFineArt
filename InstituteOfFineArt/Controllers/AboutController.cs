070499baf5e75dd030c6a52c88d302a848e5ba9c
hoangson33 <son.pnh38@aptechlearning.edu.vn>
2021-06-14 21:36:58 +0700

    layout user

---
+++ InstituteOfFineArt/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituteOfFineArt.Controllers
+{
+    [Route("about")]
+    public class AboutController : Controller
+    {
+        [Route("about")]
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ InstituteOfFineArt/Controllers/IndexController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituteOfFineArt.Controllers
+{
+    [Route("index")]
+    public class IndexController : Controller
+    {
+        [Route("index")]
+        [Route("~/")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
