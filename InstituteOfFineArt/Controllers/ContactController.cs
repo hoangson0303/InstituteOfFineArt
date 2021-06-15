82addd39c86bacb8fba6a0f2affa9639fe769cde
acer <cuong.trn37@aptechlearning.edu.vn>
2021-06-15 16:33:41 +0700

    tách file herder main and footer school layout

---
+++ InstituteOfFineArt/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituteOfFineArt.Controllers
+{
+    [Route("contant")]
+    public class ContactController : Controller
+    {
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ InstituteOfFineArt/Controllers/SchoolController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstituteOfFineArt.Controllers
+{
+    [Route("school")]
+    public class SchoolController : Controller
+    {
+        [Route("school")]
+        public IActionResult School()
+        {
+            return View();
+        }
+    }
+}
